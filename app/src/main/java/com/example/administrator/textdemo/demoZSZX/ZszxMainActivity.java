607b3902a7b9a7cdbac135f02ab880a5253e556d
weixiaoming666 <307357947@qq.com>
2018-11-12 18:43:43 +0800

    购物代码

--- app/src/main/java/com/example/administrator/textdemo/demoZSZX/ZszxMainActivity.java
+++ app/src/main/java/com/example/administrator/textdemo/demoZSZX/ZszxMainActivity.java
@@ -9,18 +9,27 @@ import android.widget.LinearLayout;
 
 import com.example.administrator.textdemo.R;
 import com.example.administrator.textdemo.demoZSZX.adapter.VpAdapter;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ZszxMainActivity extends ZSZXBaseActivity {
     private ViewPager vp_guide;
     private VpAdapter vpAdapter;
-    private static int[] imgs = {R.drawable.add_task_more, R.drawable.ic_account_box_black_24dp, R.drawable.blue_duigou};
+    List<Integer> images = new ArrayList<>()  ;
+    List<String> title = new ArrayList<>()  ;
+    private  int[] imgs = {R.drawable.add_task_more, R.drawable.ic_account_box_black_24dp, R.drawable.blue_duigou};
+    private  String[] titles = {"666", "777", "888"};
     private ArrayList<ImageView> imageViews;
     private ImageView[] dotViews;//小圆点
     private VpAdapter adapter;
     private LinearLayout dot_Layout;
     private ViewPager vp;
+    private Banner banner;
 
 
     @Override
@@ -29,6 +38,36 @@ public class ZszxMainActivity extends ZSZXBaseActivity {
         setViewLoad(R.layout.activity_zszx_main, R.id.ll_base_title, R.id.ll_content);
         setBaseTitle("测试demo首页");
         initView();
+        initBinner();
+    }
+
+    private void initBinner() {
+         banner = (Banner) findViewById(R.id.banner);
+        title.addAll(Arrays.asList(titles));
+        images.add(0,imgs[0]);
+        images.add(1,imgs[1]);
+        images.add(2,imgs[2]);
+        //设置banner样式
+
+         //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+
+        banner.setImages(Arrays.asList(imgs));
+        //设置banner动画效果
+         banner.setBannerAnimation(Transformer.DepthPage);
+        // 设置标题集合（当banner样式有显示title时）
+
+         //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(1500);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+
+
     }
 
     private void initView() {
@@ -78,6 +117,7 @@ public class ZszxMainActivity extends ZSZXBaseActivity {
             layout.addView(imageView);//添加到布局里面显示
         }
     }
+
     private void initImages() { //设置每一张图片都填充窗口
         ViewPager.LayoutParams mParams = new ViewPager.LayoutParams();
         imageViews = new ArrayList<ImageView>();
